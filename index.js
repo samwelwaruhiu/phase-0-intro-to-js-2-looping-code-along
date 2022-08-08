a89777787395745f6b274bb6d25797976397c8dc
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-08 14:48:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(stringName, eventName){
+    let thankYou = []
+    for(let i = 0; i < stringName.length; i++){
+        thankYou.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYou;
+}
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+}
